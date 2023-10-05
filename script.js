8d9f7d62fc473ae3c91f3700749b953acec7f7b0
Sikriwal9570 <128585704+Sikriwal9570@users.noreply.github.com>
2023-10-05 20:54:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let levelElement=document.getElementById("level");
+let levelcount = 1;
+while(true){
+	
+	if(levelElement.tagName === "HTML"){
+		break;
+	}
+	if(levelcount>1000){
+		break;
+	}
+	levelElement = levelElement.parentNode;
+	levelcount++;
+	
+}
+// alert("ojihugyvhb");
+// console.log(levelcount)
+alert(`The level of the element is: ${levelcount}`);
